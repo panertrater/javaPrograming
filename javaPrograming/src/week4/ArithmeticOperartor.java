3b997f4cb9e14696c2e73e07be27bf3b85a381bf
jiwon <jiwon@BOOK-T0NU75B1B4>
2024-03-28 13:02:04 +0900

    제출했습니다

---
+++ javaPrograming/src/week4/ArithmeticOperartor.java
@@ -0,0 +1,24 @@
+package week4;
+
+import java.util.*;
+
+public class ArithmeticOperartor {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("정수를 입력하시오:");
+		int time= scanner.nextInt();
+		int second = time % 60;
+		int minute = (time/60) / 60;
+		int hour = (time/60) / 60;
+		
+		System.out.println(time +"초는");
+		System.out.println(hour+"시간,");
+		System.out.println(minute + "분");
+		System.out.println(second +"초입니다");
+		
+		scanner.close();
+		
+		
+	}
+}
