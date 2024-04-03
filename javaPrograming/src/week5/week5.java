597375bf251591bd3f83d0134b49f58035330c5c
jiwon <jiwon@BOOK-T0NU75B1B4>
2024-04-03 11:46:18 +0900

    제출했습니다

---
+++ javaPrograming/src/week5/week5.java
@@ -0,0 +1,17 @@
+package week5;
+
+public class week5 {
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 5;
+		
+		if(a > b) {
+			System.out.println("참입니다");
+			
+		}else {
+			System.out.println("휴강입니다");
+		}
+		
+	}
+}
